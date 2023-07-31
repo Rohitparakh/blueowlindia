7d1d6c89c55dddcc974ff2bf66d239ad5dceb837
Rohit Parakh <rohitparakh4@gmail.com>
2023-07-31 17:18:13 +0530

    close mobile menu on link click

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var mobileMenu= document.getElementsByClassName("ekit-menu-nav-link");
+
+for (var i=0; i < mobileMenu.length; i++) {
+    mobileMenu[i].addEventListener("click", ()=>{
+document.querySelector("#ekit-megamenu-criativo-menus > div > button").click();
+    });
+  }
